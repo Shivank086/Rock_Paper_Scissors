7bad6b5411350d72a99be7d4edc90a9308464f47
Shivank Gupta <gshivank086@gmail.com>
2023-09-22 23:04:22 -0400

    git commit master

--- main.py
+++ main.py
@@ -4,27 +4,53 @@ import random
 import math
 pygame.init()
 
-SCREEN_WIDTH = 1500
-SCREEN_HEIGHT = 900
-x = 1
-y = 1
-X_SPEED = 3
-Y_SPEED = 4
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
+Rx = 8
+Ry = 6
+
+Px = -4
+Py = -9
+
+Sx = -1
+Sy = 10
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("First Game")
+def switch(fir, sec):
+    z=fir
+    fir=-sec
+    sec=z
+    return fir, sec
+
+def move(rect, x, y):
+    if rect.left<0 or rect.right>SCREEN_WIDTH:
+        x = -x
+    if rect.top<0 or rect.bottom>SCREEN_HEIGHT:
+        y = -y
+    rect.left += x
+    rect.top += y
+    return x, y
+
+def check(rect1, rect2):
+    if rect1.top<rect2.bottom and rect1.bottom>rect2.top and rect1.right>rect2.left and rect1.left<rect2.right:
+        return True
+    return False
+
+dim = 100
+ROCK = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'ROCK.png')), (dim, dim))
+rect_R = ROCK.get_rect()
+rect_R.topleft = (dim, dim)
 
 
-ROCK_IMAGE = pygame.image.load(os.path.join('assets', 'ROCK.png'))
-ROCK = pygame.transform.scale(ROCK_IMAGE, (25, 25))
-rect = ROCK.get_rect()
-"""
-PAPER_IMAGE = pygame.image.load(os.path.join('assets', 'PAPER.png'))
-PAPER = pygame.transform.scale(PAPER_IMAGE, (25, 25))
-SCISSORS_IMAGE = pygame.image.load(os.path.join('assets', 'SCISSORS.png'))
-SCISSORS = pygame.transform.scale(SCISSORS_IMAGE, (25, 25))
-"""
-FPS = 60
+PAPER = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'PAPER.png')), (dim, dim))
+rect_P = PAPER.get_rect()
+rect_P.topleft = (SCREEN_WIDTH-dim, SCREEN_HEIGHT-dim)
 
+SCISSORS = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'SCISSORS.png')), (dim, dim))
+rect_S = SCISSORS.get_rect()
+rect_S.topleft = (SCREEN_WIDTH-dim, dim)
+
+FPS = 30
 
 clock = pygame.time.Clock()
 run = True
@@ -34,19 +60,29 @@ while run:
             run=False
 
         
-        
-    if rect.left<0 or rect.right>SCREEN_WIDTH:
-        x *=-1
-        X_SPEED = x*random.randint(1, 4)
-        Y_SPEED = y*(5-X_SPEED)
-    if rect.top<0 or rect.bottom>SCREEN_HEIGHT:
-        y *=-1
-        X_SPEED = x*random.randint(1, 4)
-        Y_SPEED = y*(5-X_SPEED)
-    rect.left += X_SPEED
-    rect.top += Y_SPEED
+    hold = move(rect_R, Rx, Ry)
+    Rx, Ry = hold[0], hold[1]
+    
+    hold = move(rect_P, Px, Py)
+    Px, Py = hold[0], hold[1]
+    
+    hold = move(rect_S, Sx, Sy)
+    Sx, Sy = hold[0], hold[1]
+
+
+    if check(rect_R, rect_S):
+        SCISSORS = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'ROCK.png')), (dim, dim))
+
+    if check(rect_S, rect_P):
+        PAPER = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'SCISSORS.png')), (dim, dim))
+
+    if check(rect_P, rect_R):
+        ROCK = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'PAPER.png')), (dim, dim))
+
     screen.fill((0, 0, 0))
-    screen.blit(ROCK, rect)
+    screen.blit(ROCK, rect_R)
+    screen.blit(PAPER, rect_P)
+    screen.blit(SCISSORS, rect_S)
     pygame.display.update()
     clock.tick(FPS)
 
