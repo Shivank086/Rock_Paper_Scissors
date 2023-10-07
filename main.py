f06f3ee66b459262336441bc66788eb9837ca566
Shivank Gupta <gshivank086@gmail.com>
2023-10-07 17:20:42 -0400

    update

--- main.py
+++ main.py
@@ -6,14 +6,7 @@ pygame.init()
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
-Rx = 8
-Ry = 6
 
-Px = -4
-Py = -9
-
-Sx = -1
-Sy = 10
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("First Game")
 def switch(fir, sec):
@@ -22,42 +15,50 @@ def switch(fir, sec):
     sec=z
     return fir, sec
 
-def move(rect, x, y):
+def move(rect, x, y, i):
+    holdx = abs(x)/x
+    holdy = abs(y)/y
     if rect.left<0 or rect.right>SCREEN_WIDTH:
-        x = -x
+        x = holdx*random.randint(1, 10)
+        y = holdy*(10-abs(x))
     if rect.top<0 or rect.bottom>SCREEN_HEIGHT:
-        y = -y
-    rect.left += x
-    rect.top += y
-    return x, y
+        y = holdy*random.randint(1, 10)
+        x = holdx*(10-abs(y))
+    varHold[keyHold[i]][2].left += x
+    varHold[keyHold[i]][2].top += y
+    varHold[keyHold[i]][3], varHold[keyHold[i]][4] = x, y
 
-def check(rect1, rect2):
-    if rect1.top<rect2.bottom and rect1.bottom>rect2.top and rect1.right>rect2.left and rect1.left<rect2.right:
-        return True
+def placeCheck(rect1, rect2):
+    if rect1.top<rect2.bottom and rect1.bottom>rect2.top and rect1.right>rect2.left and rect1.left<rect2.right: return True
+    return False
+
+def typeCheck(img1, img2):
+    if img1==img2+1 or img1 == img2-2:return True
     return False
 
 
-
 dim = 100
-ROCK = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'ROCK.png')), (dim, dim))
-rect_R = ROCK.get_rect()
-rect_R.topleft = (dim, dim)
-
-
-PAPER = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'PAPER.png')), (dim, dim))
-rect_P = PAPER.get_rect()
-rect_P.topleft = (SCREEN_WIDTH-dim, SCREEN_HEIGHT-dim)
-
-SCISSORS = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'SCISSORS.png')), (dim, dim))
-rect_S = SCISSORS.get_rect()
-rect_S.topleft = (SCREEN_WIDTH-dim, dim)
-
 FPS = 30
 varHold = {
-    "Rock": [3, ROCK, rect_R, Rx, Ry],
-    "Paper": [2, PAPER, rect_P, Px, Py],
-    "Scissors": [1, SCISSORS, rect_S, Sx, Sy]
+    "Rock": [3, pygame.transform.scale(pygame.image.load(os.path.join('assets', 'ROCK.png')), (dim, dim)), 
+             pygame.transform.scale(pygame.image.load(os.path.join('assets', 'ROCK.png')), (dim, dim)).get_rect(), 8, 7],
+    "Paper": [2, pygame.transform.scale(pygame.image.load(os.path.join('assets', 'PAPER.png')), (dim, dim)), 
+              pygame.transform.scale(pygame.image.load(os.path.join('assets', 'PAPER.png')), (dim, dim)).get_rect(), -6, -9],
+    "Scissors": [1, pygame.transform.scale(pygame.image.load(os.path.join('assets', 'SCISSORS.png')), (dim, dim)), 
+                 pygame.transform.scale(pygame.image.load(os.path.join('assets', 'SCISSORS.png')), (dim, dim)).get_rect(), -1, 14]
 }
+keyHold = list(varHold.keys())
+varHold[keyHold[0]][2].topleft = (dim, dim)
+varHold[keyHold[1]][2].topleft = (SCREEN_WIDTH-dim, SCREEN_HEIGHT-dim)
+varHold[keyHold[2]][2].topleft = (SCREEN_WIDTH-dim, dim)
+
+def listCheck():
+    for i in range(len(keyHold)-2):
+        for j in range(len(keyHold)-(i+1)):
+            if placeCheck(varHold[keyHold[i+1]][2], varHold[keyHold[j+i+1]][2]) and typeCheck(varHold[keyHold[i+1]][0], varHold[keyHold[j+i+1]][0]):
+                varHold[keyHold[i+1]][1] = varHold[keyHold[j+i+1]][1]
+                varHold[keyHold[i+1]][0] = varHold[keyHold[j+i+1]][0]
+
 
 clock = pygame.time.Clock()
 run = True
@@ -66,29 +67,14 @@ while run:
         if event.type == pygame.QUIT:
             run=False
 
-        
-    hold = move(rect_R, Rx, Ry)
-    Rx, Ry = hold[0], hold[1]
-    
-    hold = move(rect_P, Px, Py)
-    Px, Py = hold[0], hold[1]
-    
-    hold = move(rect_S, Sx, Sy)
-    Sx, Sy = hold[0], hold[1]
 
-
-    for i in range(len(varHold)-1):
-        
-        if i == len(varHold)-1: val = check(varHold[i+1][0], varHold[i+2][0])
-        else: check(varHold[i+1][0], varHold[0][0])
-        if val:
-            varHold[i+1][0] = varHold[i+2][1]
+    listCheck()
 
 
     screen.fill((0, 0, 0))
-    screen.blit(ROCK, rect_R)
-    screen.blit(PAPER, rect_P)
-    screen.blit(SCISSORS, rect_S)
+    for i in range(len(keyHold)):
+        screen.blit(varHold[keyHold[i]][1], varHold[keyHold[i]][2])
+        move(varHold[keyHold[i]][2], varHold[keyHold[i]][3], varHold[keyHold[i]][4], i)
     pygame.display.update()
     clock.tick(FPS)
 
